7eb4d521d8af3d7d5de432ebd00eddabc4cfe94f
Angel Garcia Marin <isc18350309@gmail.com>
2022-03-01 10:09:47 -0600

    Primer Hola Mundo

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Saludo from "./Saludo"
+const App= () => {
+  return (
+    <div className="App">
+      <Saludo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Saludo.jsx
@@ -0,0 +1,5 @@
+const Saludo =() => {
+    return <h1>Mi segundo Hola Mundo!! </h1>;  
+};
+
+export default Saludo;
